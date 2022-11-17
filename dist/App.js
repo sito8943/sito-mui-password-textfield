1b47cc9a54a8f1519d035abb778bfec1b5703882
Sito8943 <sito8943@gmail.com>
2022-11-16 21:58:50 -0500

    to publish

--- dist/App.js
+++ dist/App.js
@@ -9,7 +9,7 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { jsx as _jsx } from "react/jsx-runtime";
 import { jsxs as _jsxs } from "react/jsx-runtime";
 
-const SitoMUIPasswordTextfield = (props) => {
+const SitoMUIPasswordTextfield = props => {
   const {
     id,
     sx,
@@ -29,63 +29,57 @@ const SitoMUIPasswordTextfield = (props) => {
     onInvalid,
     placeholder,
     onIconClick,
-    onMouseDown,
-    defaultValue,
+    defaultValue
   } = props;
   const [showPassword, setShowPassword] = useState(false);
   const [localValue, setLocalValue] = useState("");
 
-  const onLocalChange = (e) => setLocalValue(e.target.value);
+  const onLocalChange = e => setLocalValue(e.target.value);
 
   const onLocalIconClick = () => setShowPassword(!showPassword);
 
-  const onLocalMouseDown = (event) => event.preventDefault();
+  const onLocalMouseDown = event => event.preventDefault();
 
   const parsedInputSx = {
     width: "100%",
-    ...inputSx,
+    ...inputSx
   };
   const parsedIconSx = {
     position: "absolute",
     right: "1%",
     top: "50%",
     transform: "translateY(-50%)",
-    ...iconSx,
+    ...iconSx
   };
-  return /*#__PURE__*/ _jsxs(Box, {
+  return /*#__PURE__*/_jsxs(Box, {
     sx: {
       position: "relative",
-      ...sx,
+      ...sx
     },
-    children: [
-      /*#__PURE__*/ _jsx(TextField, {
-        id: id,
-        name: name,
-        error: error,
-        label: label,
-        color: color,
-        variant: variant,
-        sx: parsedInputSx,
-        disabled: disabled,
-        required: required,
-        className: className,
-        placeholder: placeholder,
-        onInput: onInput || null,
-        value: value || localValue,
-        defaultValue: defaultValue,
-        onInvalid: onInvalid || null,
-        onChange: onChange || onLocalChange,
-        type: showPassword ? "text" : "password",
-      }),
-      /*#__PURE__*/ _jsx(IconButton, {
-        sx: parsedIconSx,
-        onClick: onIconClick || onLocalIconClick,
-        onMouseDown: onMouseDown || onLocalMouseDown,
-        children: showPassword
-          ? /*#__PURE__*/ _jsx(VisibilityOff, {})
-          : /*#__PURE__*/ _jsx(Visibility, {}),
-      }),
-    ],
+    children: [/*#__PURE__*/_jsx(TextField, {
+      id: id,
+      name: name,
+      error: error,
+      label: label,
+      color: color,
+      variant: variant,
+      sx: parsedInputSx,
+      disabled: disabled,
+      required: required,
+      className: className,
+      placeholder: placeholder,
+      onInput: onInput || null,
+      value: value || localValue,
+      defaultValue: defaultValue,
+      onInvalid: onInvalid || null,
+      onChange: onChange || onLocalChange,
+      type: showPassword ? "text" : "password"
+    }), /*#__PURE__*/_jsx(IconButton, {
+      sx: parsedIconSx,
+      onClick: onIconClick || onLocalIconClick,
+      onMouseDown: onLocalMouseDown,
+      children: showPassword ? /*#__PURE__*/_jsx(VisibilityOff, {}) : /*#__PURE__*/_jsx(Visibility, {})
+    })]
   });
 };
 
@@ -103,26 +97,17 @@ SitoMUIPasswordTextfield.defaultProps = {
   onChange: undefined,
   onInvalid: undefined,
   onIconClick: undefined,
-  onMouseDown: undefined,
   value: undefined,
-  default: undefined,
+  defaultValue: undefined,
   sx: {},
   inputSx: {},
-  iconSx: {},
+  iconSx: {}
 };
 SitoMUIPasswordTextfield.propTypes = {
   id: PropTypes.string,
   name: PropTypes.string,
   label: PropTypes.string,
-  color: PropTypes.oneOf([
-    "primary",
-    "secondary",
-    "error",
-    "info",
-    "success",
-    "warning",
-    "string",
-  ]),
+  color: PropTypes.oneOf(["primary", "secondary", "error", "info", "success", "warning", "string"]),
   variant: PropTypes.oneOf(["filled", "outlined", "standard"]),
   className: PropTypes.string,
   placeholder: PropTypes.string,
@@ -132,29 +117,10 @@ SitoMUIPasswordTextfield.propTypes = {
   onChange: PropTypes.func,
   onInvalid: PropTypes.func,
   onIconClick: PropTypes.func,
-  onMouseDown: PropTypes.func,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  sx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-  inputSx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
-  iconSx: PropTypes.oneOfType([
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])
-    ),
-    PropTypes.func,
-    PropTypes.object,
-  ]),
+  sx: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object]),
+  inputSx: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object]),
+  iconSx: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])), PropTypes.func, PropTypes.object])
 };
 export default SitoMUIPasswordTextfield;
